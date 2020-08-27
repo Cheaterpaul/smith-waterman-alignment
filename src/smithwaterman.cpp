a70adb5147230302188e886cc284aeb2d6df1cf9
cheaterpaul <dev@paube.de>
2020-08-27 19:21:23 +0200

    faster

--- src/smithwaterman.cpp
+++ src/smithwaterman.cpp
@@ -22,21 +22,21 @@ int compare(int x,int y) {
     int maxValue = 0;
     
     std::vector<int> collum1(51, 0);
-    std::vector<int> collum2(51);
+    std::vector<int> collum2(51, 0);
     for (size_t i = 1; i <= 50;++i) {
+        int a = collum1[1];
         for (size_t j = 1; j <= 50;++j) {
-            int value = collum1[j]-1>collum2[j-1]-1?collum1[j]-1:collum2[j-1]-1;
-            if (value <= collum1[j-1]+2) {
-                int value2 = collum1[j-1] + (file1[x+i] == file2[y+j] ? 2 : -1);
-                if (value2 > value) {
-                    value = value2;
+            int value = std::max(a-1,collum1[j-1]-1);
+            int value2 = (a = collum1[j-1]) + (file1[x+i] == file2[y+j] ? 2 : -1);
+            if (value2 > value) {
+                value = value2;
+            }
+            if (value > 0) {
+                collum2[j] = value;
+                if (maxValue < value) {
+                    maxValue = value;
                 }
             }
-            collum2[j] = value;
-            if (maxValue < value) {
-                maxValue = value;
-            }
-            
         }
         collum1.swap(collum2);
 
